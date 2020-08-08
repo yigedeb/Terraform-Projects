a6b87f15ce60a6e020ba1075dc6894fa739f2814
yigedeb <yigedeb@outlook.com>
2020-08-08 09:27:48 -0400

    Update main.tf

--- 12-CI-CD-Pipelines/Azure-Devops/AKS/main.tf
+++ 12-CI-CD-Pipelines/Azure-Devops/AKS/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_kubernetes_cluster" "CloudSkillsAKS" {
   default_node_pool {
     name = "default"
     node_count = 1
-    vm_size = "Standard_D2s_v3"
+    vm_size = "Standard_DS1_v2"
   }
   service_principal {
     client_id     = var.clientID
